02242bdef5e56c285c869e1c6f55ae0e25570afd
Grubosch <max@radenhaeuser.de>
2025-07-01 19:56:34 +0200

    "changes"

--- goes_db.py
+++ goes_db.py
@@ -75,6 +75,10 @@ def main():
     ensure_download_dir()
 
     engine = create_engine(DB_URI)
+    with engine.connect() as conn:
+        result = conn.execute("SELECT table_schema, table_name FROM information_schema.tables WHERE table_name = 'particle_flux';")
+        for row in result:
+            print(row)
     conn=engine.connect()
 
     for sat in GOES_SATELLITES:
